72fd8c11be30c90038cabd3375f5624ca24e1f93
nospame <mapson.evan@gmail.com>
2022-04-29 09:55:34 -0700

    add axios, add basic recipes index page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import RecipesIndex from '../views/RecipesIndex.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/recipes',
+    name: 'recipes-index',
+    component: RecipesIndex
   },
   {
     path: '/about',
---
+++ src/views/RecipesIndex.vue
@@ -0,0 +1,39 @@
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      message: "Welcome to the Recipes Index Page",
+      recipes: []
+    };
+  },
+  created: function () {
+    this.indexRecipes()
+  },
+  methods: {
+    indexRecipes: function () {
+      console.log('in the index function')
+      axios.get('/recipes.json')
+        .then(response => {
+          this.recipes = response.data
+          console.log(response.data)
+        })
+    }
+  },
+};
+</script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <div v-for="recipe in recipes">
+      <h2>{{ recipe.name }}</h2>
+      <p>{{ recipe.description }}</p>
+      <hr />
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
