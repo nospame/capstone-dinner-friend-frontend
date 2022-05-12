3464f81f314cc205d6485b0905ba7831258fd051
nospame <mapson.evan@gmail.com>
2022-05-12 11:17:16 -0700

    add favorite recipes sort by name or date added

--- src/views/FavoriteRecipes.vue
+++ src/views/FavoriteRecipes.vue
@@ -5,7 +5,8 @@ export default {
   data: function () {
     return {
       message: "Welcome to the Favorites Page",
-      favorites: []
+      favorites: [],
+      sort: ''
     };
   },
   created: function () {
@@ -22,16 +23,66 @@ export default {
     showRecipe: function (id) {
       this.$router.push(`/recipes/${id}`)
     },
+    sortFavoriteRecipes: function (sort) {
+      switch (sort) {
+        case 'old':
+          this.favorites = this.favorites.sort((a, b) => {
+            let da = new Date(a.created_at),
+              db = new Date(b.created_at);
+            return da - db;
+          })
+          break
+        case 'new':
+          this.favorites = this.favorites.sort((a, b) => {
+            let da = new Date(a.created_at),
+              db = new Date(b.created_at);
+            return db - da;
+          })
+          break
+        case 'asc':
+          this.favorites = this.favorites.sort((a, b) => {
+            let ra = a.recipe.name.toLowerCase(),
+              rb = b.recipe.name.toLowerCase();
+            if (ra < rb) {
+              return -1;
+            }
+            if (ra > rb) {
+              return 1;
+            }
+            return 0;
+          });
+          break
+        case 'desc':
+          this.favorites = this.favorites.sort((a, b) => {
+            let ra = a.recipe.name.toLowerCase(),
+              rb = b.recipe.name.toLowerCase();
+            if (rb < ra) {
+              return -1;
+            }
+            if (rb > ra) {
+              return 1;
+            }
+            return 0;
+          });
+          break
+      }
+    }
   },
 };
 </script>
 
 <template>
   <div class="favorites">
+    <label for="sort">Sort by:</label>
+    <select id="sort" v-model="sort">
+      <option value="new">Newest</option>
+      <option value="old">Oldest</option>
+      <option value="asc">A - Z</option>
+      <option value="desc">Z - A</option>
+    </select>
+    <button class="btn btn-primary btn-sm" v-on:click="sortFavoriteRecipes(sort)">Sort and Filter</button>
     <div v-for="favorite in favorites">
       <h2>{{ favorite.recipe.name }}</h2>
-      <!-- <span v-if="!favorite.has_made" class="badge bg-info">Not Cooked</span>
-      <span v-else="!!favorite.has_made" class="badge bg-secondary">Cooked!</span> -->
       <p>{{ favorite.recipe.description }}</p>
       <button class="btn btn-primary position-relative" v-on:click="showRecipe(favorite.recipe.id)">Recipe Details <span
           class="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle"
