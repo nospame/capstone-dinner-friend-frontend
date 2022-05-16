c3a70cd725d3248eddf1bb4acaa7825e412ac6de
nospame <mapson.evan@gmail.com>
2022-05-16 13:55:54 -0700

    work on responsive navbar

--- src/App.vue
+++ src/App.vue
@@ -17,38 +17,44 @@ export default {
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <a class="navbar-brand" href="/">Dinner Party's Friend</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
           <li class="nav-item">
-            <router-link class="nav-link active" aria-current="page" to="/recipes">Search Recipes</router-link>
-          </li>
-          <li v-if="user.loggedIn" class="nav-item">
-            <router-link class="nav-link" to="/favorites/recipes">Favorites</router-link>
-          </li>
-          <li v-if="user.loggedIn" class="nav-item">
-            <router-link class="nav-link" to="/favorites/searches">Saved Searches</router-link>
-          </li>
-          <li v-if="!user.loggedIn" class="nav-item">
-            <router-link class="nav-link" to="/users/new">Sign Up</router-link>
-          </li>
-          <li v-if="!user.loggedIn" class="nav-item">
-            <router-link class="nav-link" to="/login">Log In</router-link>
-          </li>
-          <li v-if="user.loggedIn" class="nav-item">
-            <router-link class="nav-link" to="/logout">Log Out</router-link>
+            <a class="nav-link" href="#">Link</a>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" to="/about">About</router-link>
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
           </li>
+          <li class="nav-item dropdown" v-if="!user.loggedIn">
+            <a class="btn btn-primary" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">
+              Sign Up
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li>
+                <router-link class="dropdown-item" to="/users/new">Create Account</router-link>
+              </li>
+              <li>
+                <router-link class="dropdown-item" to="/login">Login</router-link>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item" v-else="user.loggedIn">
+            <router-link class="btn btn-secondary" to="/logout">Logout</router-link>
+          </li>
+
         </ul>
+
       </div>
     </div>
   </nav>
+  {{ user.LoggedIn }}
   <div class="container">
     <router-view />
   </div>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import "bootstrap/dist/css/bootstrap.min.css"
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from "axios"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://nospame-capstone-api.herokuapp.com";
 
